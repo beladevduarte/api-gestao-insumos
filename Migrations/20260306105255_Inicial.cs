e8f20bb2da52d6139486a21e1e6894f3423f5a33
Alexandre Roberto Neme Kulpel <alexandre.kulpel@zcksoftware.com.br>
2026-03-06 07:54:22 -0300

    adiciona pacote de design e migration inicial do banco de dados

---
+++ Migrations/20260306105255_Inicial.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZCK.OrdensProducao.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ordens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ordens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItensReceita",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrdemProducaoId = table.Column<int>(type: "int", nullable: false),
+                    NomeInsumo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantidade = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItensReceita", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItensReceita_Ordens_OrdemProducaoId",
+                        column: x => x.OrdemProducaoId,
+                        principalTable: "Ordens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensReceita_OrdemProducaoId",
+                table: "ItensReceita",
+                column: "OrdemProducaoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItensReceita");
+
+            migrationBuilder.DropTable(
+                name: "Ordens");
+        }
+    }
+}
